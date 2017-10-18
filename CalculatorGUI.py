950e3c7f8f07a3e30a7a8cf38c334b8a5d4cd69c
adammertzenich <adam.mertzenich@decorahvikings.k12.ia.us>
2017-10-18 11:11:35 -0500

    Updated Version

--- CalculatorGUI.py
+++ CalculatorGUI.py
@@ -8,7 +8,7 @@ import Tkinter,tkMessageBox,math,redis
 Name = 'Calculator Project'
 Description = 'This project is to create a GUI based calculator using Tkinter'
 Repository = 'https://github.com/adammertzenich/PyCalcGUI'
-Version = '2.10.3' # Major, Minor, Patch
+Version = 'R1.0.0' # Major, Minor, Patch
 Author = 'Adam Mertzenich'
 def about(): # about button fires about() which displays the about message box
     tkMessageBox.showinfo("About", "Project Name: " + Name + "\n" + "Author: " + Author + "\n" + "Description: " + Description + "\n" + "Version: " + Version + "\n" + "Repository: " + Repository)
--- README.md
+++ README.md
@@ -1,22 +1,3 @@
 # Python Calculator GUI
 
-A project using tkinter to build a calculator with a GUI.
-
-My first large gui project.
-
----
-
-The <a href=https://github.com/adammertzenich/PyCalcGUI/tree/dev>DEV BRANCH</a> is updated more often than the master
-
----
-
-Current Imports:
-
-<p>
-
-Tkinter <br />
-tKMessageBox <br />
-Future Division<br />
-Math<br />
-
-</p>
+This is a branch of the calculator that aims to use redis to store data.
